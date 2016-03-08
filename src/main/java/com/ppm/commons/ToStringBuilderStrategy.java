cf4fd8199f28431870429ffdf67579a88c2de3a4
Pedro T. Oliveira <pedro.oliveira20@gmail.com>
2016-03-07 23:14:35 -0300

    Starting implementation

---
+++ src/main/java/com/ppm/commons/ToStringBuilderStrategy.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) - UOL Inc, Todos os direitos reservados
+ *
+ * Este arquivo e uma propriedade confidencial do Universo Online Inc. Nenhuma
+ * parte do mesmo pode ser copiada, reproduzida, impressa ou transmitida por
+ * qualquer meio sem autorizacao expressa e por escrito de um representante
+ * legal do Universo Online Inc.
+ *
+ * All rights reserved
+ *
+ * This file is a confidential property of Universo Online Inc. No part of this
+ * file may be reproduced or copied in any form or by any means without written
+ * permisson from an authorized person from Universo Online Inc.
+ *
+ */
+package com.ppm.commons;
+
+/**
+ *
+ * @author pedrotoliveira
+ */
+interface ToStringBuilderStrategy {
+
+    <T> void appendValue(T o, StringBuilder builder);
+}
